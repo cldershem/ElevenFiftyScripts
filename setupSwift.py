03de5d7b9311eb4e23a5f0e62c79d50503d86191
Cameron Dershem <cldershem@gmail.com>
2014-10-02 13:44:39 -0400

    should work.

--- setupSwift.py
+++ setupSwift.py
@@ -10,18 +10,67 @@ Checks to make sure machine (!Person) is ready for the iOS w/ Swift class.
 :license: see TOPMATTER
 :source: github.com/cldershem/repo
 """
-import os
 import platform
+import subprocess
+import os.path
 
 
-def check_os_version():
-    print(os.system("uname -r"))
-    print(platform.mac_ver())
+"""
+# def can_run_xcode():
+    # if check_os_version:
+        # print("you're good.")
+        # check_xcode_version()
+    # else:
+        # print("No bueno.")
+        """
 
 
-def check_xcode_version():
-    pass
+def os_is_mavericks():
+    version = platform.mac_ver()
+    if version == "10.9" or version == "10.10":
+        return True
+    else:
+        return False
+
+
+def xcode_is_installed():
+    if os.path.isfile("/Applications/Xcode.app"):
+        return True
+    else:
+        return False
+
+
+def xcode_version_is_right():
+    command = "xcodebuild -version"
+    version = subprocess.check_output(command.split())
+    version_num = version.split()[1]
+    if version_num >= "6.0":
+        return True
+    else:
+        return False
+
+
+def install_xcode():
+    print("installing xcode")
+
+
+def update_osx():
+    print("updating osx")
 
 
 if __name__ == '__main__':
-    check_os_version()
+    print("begin")
+    if xcode_is_installed():
+        print("is installed")
+        if xcode_version_is_right():
+            print("is right version")
+        else:
+            print("not installed")
+            if os_is_mavericks():
+                print("right osx")
+                install_xcode()
+            else:
+                print("wrong osx")
+                update_osx()
+                install_xcode()
+    print("done")
